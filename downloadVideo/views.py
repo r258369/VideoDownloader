07784fe6bd79a49b8cf9f1b328e0bda1d01beb18
r258369 <ratulhalderjsr007@gmail.com>
2025-08-31 14:19:46 +0600

    Update views.py

--- downloadVideo/views.py
+++ downloadVideo/views.py
@@ -1,13 +1,10 @@
-
-
 from django.shortcuts import render
 import os
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from yt_dlp import YoutubeDL
 
-YOUTUBE_EMAIL = "xyz328719@gmail.com"
-YOUTUBE_PASSWORD = "ratul@369"  # use app password if 2FA is enabled
+
 
 def home(request):
     return render(request, 'index.html')
@@ -18,23 +15,16 @@ def download_media(request):
         url = request.POST.get("url")
         media_type = request.POST.get("type", "video")  # video or audio
 
-        if not url:
-            return JsonResponse({"error": "URL is required"}, status=400)
-
         try:
-            # yt-dlp options
             ydl_opts = {
                 'format': 'bestaudio/best' if media_type == "audio" else 'best',
                 'outtmpl': 'downloads/%(title)s.%(ext)s',
-                'username': YOUTUBE_EMAIL,
-                'password': YOUTUBE_PASSWORD,
             }
-
             with YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(url, download=True)
                 filename = ydl.prepare_filename(info)
 
-            # Serve file back
+            # Serve file back as download
             with open(filename, 'rb') as f:
                 response = HttpResponse(f.read(), content_type='application/octet-stream')
                 response['Content-Disposition'] = f'attachment; filename="{os.path.basename(filename)}"'
